82ecca35069ac4079891dffee6143c2133f4bb2f
Blain Crawford <blaincrawford@gmail.com>
2022-03-04 12:05:16 -0500

    about to look into more scrolling options

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import './styles.css';
 import apple from './images/apple.jpg';
 import bald from './images/Bald.jpg';
 import blainFunnyFace from './images/blain-funny-face.jpg';
@@ -15,8 +15,21 @@ import solar from './images/solar.jpg';
 const pictureScroller = document.querySelector('#pictures');
 const leftScroller = document.querySelector('#picture-scroll-left');
 const rightScroller = document.querySelector('#picture-scroll-right');
-let imageArray = [apple, bald, blainFunnyFace, candle, cheers, christmas, freshHair, froPoint, hardhat, kiss, sneak, snow, solar];
-
+let imageArray = [
+  apple,
+  bald,
+  blainFunnyFace,
+  candle,
+  cheers,
+  christmas,
+  freshHair,
+  froPoint,
+  hardhat,
+  kiss,
+  sneak,
+  snow,
+  solar,
+];
 
 const populatePictureFrom = (() => {
   for (let i = 0; i < imageArray.length; i += 1) {
@@ -28,7 +41,7 @@ const populatePictureFrom = (() => {
 })();
 
 const scrollPictures = (() => {
-  let pictures = document.querySelectorAll('.picture')
+  let pictures = document.querySelectorAll('.picture');
   let scrollAmount = 0;
   let clickOn = false;
 
@@ -36,42 +49,37 @@ const scrollPictures = (() => {
     clickOn = false;
     console.log(clickOn);
   }
-  
+
   function scrollRight() {
     clickOn = true;
-    console.log(clickOn)
     if (clickOn === true) {
-      if (scrollAmount < 1500) {
-        scrollAmount += 20;
-        console.log(scrollAmount)
+      while (scrollAmount < 1200) {
+        setTimeout(scrollAmount += 20, 3000);
+        console.log(scrollAmount);
         pictures.forEach((picture) => {
-          picture.style.cssText = `transform: translate(${scrollAmount}px);`
-        });
-      }
-    }
-  }
-  
-  function scrollLeft() {
-    clickOn = true;
-    console.log(clickOn)
-    if (clickOn === true) {
-      if (scrollAmount > -1500) {
-        scrollAmount -= 20;
-        console.log(scrollAmount)
-        pictures.forEach((picture) => {
-          picture.style.cssText = `transform: translate(${scrollAmount}px);`
+          picture.style.cssText = `transform: translate(${scrollAmount}px);`;
         });
       }
     }
   }
 
-  return {scrollLeft, scrollRight, turnOffScroll};
+  function scrollLeft() {
+    clickOn = true;
+    if (clickOn === true) {
+      while (scrollAmount > -1200) {
+        setTimeout(scrollAmount -= 20, 3000);
+        console.log(scrollAmount);
+        pictures.forEach((picture) => {
+          picture.style.cssText = `transform: translate(${scrollAmount}px);`;
+        });
+      }
+    }
+  }
+
+  return { scrollLeft, scrollRight, turnOffScroll };
 })();
 
 leftScroller.addEventListener('mousedown', scrollPictures.scrollLeft, false);
 rightScroller.addEventListener('mousedown', scrollPictures.scrollRight, false);
 leftScroller.addEventListener('mouseup', scrollPictures.turnOffScroll, false);
 rightScroller.addEventListener('mouseup', scrollPictures.turnOffScroll, false);
-
-
-
